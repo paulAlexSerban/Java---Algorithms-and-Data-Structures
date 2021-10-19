bd50632147e42cbcccf74f708f7f5b7f4763bd1d
paulAlexSerban <paul.alex.serban@gmail.com>
2021-10-19 16:26:09 +0300

    JAaDS_04 | ADDED | SpeedConverter and MegaByteConverter coding exercies

---
+++ BasicJava/codingExercies/MegaBytesConverter/Main.java
@@ -0,0 +1,7 @@
+package BasicJava.codingExercies.MegaBytesConverter;
+
+public class Main {
+    public static void main(String[] args) {
+        MegaByteConverter.printMegaBytesAndKiloBytes(2500);
+    }
+}
